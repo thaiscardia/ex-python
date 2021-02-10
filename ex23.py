14094e61c53cd839d0a8584c645efdcc157ec096
Thais Cardia <thaiscardia93@gmail.com>
2021-02-10 17:50:12 -0300

    exercício 23 resolvido

--- ex23.py
+++ ex23.py
@@ -1,1 +1,10 @@
 #Crie um programa que leia o nome de uma pessoa e diga se ela tem "Silva" no nome.
+n = input("Digite seu nome: ")
+sp = n.split()
+search = "Silva"
+
+if search in sp:
+    print("Você possui Silva no seu nome.")
+
+else:
+    print("Você não possui Silva no seu nome.")
