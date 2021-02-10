8000fa6038db4bdc518b55fb8ec9a314b1147b4f
Thais Cardia <thaiscardia93@gmail.com>
2021-02-10 15:45:03 -0300

    exercício 20 resolvido

--- ex20.py
+++ ex20.py
@@ -3,3 +3,10 @@
 - O nome com todas as letras minúsculas;
 - Quantas letras tem ao todo;
 - Quantas letras tem o primeiro nome. """
+
+n = input("Digite seu nome completo: ")
+ma = n.upper()
+mi = n.lower()
+np = n.replace(' ', '')
+sp = n.split()
+print("Seu nome em maiúsculo é {}, em minúsculo {}, tem {} caracteres ao todo e seu primeiro nome possui {} letras".format(ma, mi, len(np),len(sp[0])))
