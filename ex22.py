bde485cd8d4c1088b39412fd619f3cb15acec019
Thais Cardia <thaiscardia93@gmail.com>
2021-02-10 17:34:28 -0300

    exercício 22 resolvido

--- ex22.py
+++ ex22.py
@@ -1,1 +1,8 @@
 #Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com o nome "Santo"
+n = input("Digite o nome da uma cidade: ")
+sp = n.split()
+
+if sp[0] == "Santo":
+    print("A cidade {} começa com o nome Santo.".format(n))
+else:
+    print("A cidade {} não começa com o nome Santo".format(n))
