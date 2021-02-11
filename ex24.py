f2b9484d5885e4ff67a674ae4d54d7038b1d8dff
Thais Cardia <thaiscardia93@gmail.com>
2021-02-11 18:45:04 -0300

    exercício 24 resolvido

--- ex24.py
+++ ex24.py
@@ -2,3 +2,8 @@
 - quantas vezes aparece a letra "A";
 - em que posição ela aparece a primeira vez;
 - em que posição ela aparece a última vez;"""
+
+f = input("Digite a frase aqui: ")
+f = f.upper()
+print("Esta frase possui {} letra(s) A".format(f.count("A")))
+print("O primeiro A da string está localizado em {} e o último em {}.".format(f.index("A"), f.rindex("A")))
