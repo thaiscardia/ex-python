08c68b0b3f47520af31ada4993525932e07347ce
Thais Cardia <thaiscardia93@gmail.com>
2021-02-14 18:24:08 -0300

    exercício 31 resolvido - maior ou menor valor

--- ex31.py
+++ ex31.py
@@ -1,1 +1,8 @@
 #Faça um programa que leia três números e mostre qual é o maior e qual é o menor.
+n1 = float(input("Digite primeiro número: "))
+n2 = float(input("Digite segundo número: "))
+n3 = float(input("Digite terceiro número: "))
+
+l = [n1, n2, n3]
+
+print("O maior número é {} e o menor número é {}".format(max(l), min(l)))
