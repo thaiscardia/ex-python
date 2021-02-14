dfa89c4aa513f404e06b93cafdae40ffff3fb078
Thais Cardia <thaiscardia93@gmail.com>
2021-02-14 18:36:14 -0300

    ex. 32 resolvido - aumentos multiplos

--- ex32.py
+++ ex32.py
@@ -1,3 +1,15 @@
 #Escreva um programa que pergunte o salário de um funcionário e calcule o valor do seu aumento. 
 # Para salários superiores a R$1250,00, calcule um aumento de 10%. 
 # Para os inferiores ou iguais, o aumento é de 15%.
+
+s = float(input("Digite o salário do funcionário: "))
+sA = 10
+sB = 15
+
+if s >= 1250:
+    s += (s * sB) / 100
+    print("Seu salário com {}% de aumento é de R${:.2f}". format(sB, s))
+else:
+    s += (s * sA) / 100
+    print("Seu salário com {}% de aumento é de R${:.2f}.".format(sA, s))
+
