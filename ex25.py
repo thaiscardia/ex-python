abd29364f28d8d59447769d662b8a745adf0b0ef
Thais Cardia <thaiscardia93@gmail.com>
2021-02-14 15:01:03 -0300

    exercício 25 resolvido

--- ex25.py
+++ ex25.py
@@ -1,1 +1,6 @@
 """Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o ultimo nome separadamente"""
+n = input("Digite seu nome completo: ")
+nSP = n.split()[0]
+rev = n.split()[-1]
+
+print("Seu primeiro nome é {} e seu último nome é {}.".format(nSP, rev))
